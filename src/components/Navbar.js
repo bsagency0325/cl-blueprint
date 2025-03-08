56e50de425871a788d5685ed6dbebf533b937168
4me7h <4573r10n@gmail.com>
2025-03-07 19:21:56 -0600

    component: navbar styles center and logo images

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,17 +2,17 @@ import Col from "react-bootstrap/esm/Col"
 import Container from "react-bootstrap/esm/Container"
 import Row from "react-bootstrap/esm/Row"
 
-import { useState } from "react";
+// import { useState } from "react";
 import {Link} from 'react-scroll';
 
 import logoNavbar from '../images/logo-lineal-white.png';
 
 const Navbar = () => {
 
-    const [visible, setVisible] = useState(true);
-    const toggleNavbar = () => {
-        setVisible(!visible);
-    }
+    // const [visible, setVisible] = useState(true);
+    // const toggleNavbar = () => {
+    //     setVisible(!visible);
+    // }
 
     return (
         <nav className="navBar">
@@ -23,7 +23,7 @@ const Navbar = () => {
                             <div className="navBar__col">
                                 <Link href="#" to="hero" smooth={true} duration={500} className="navBar__brand"><img className="image__logo-navbar" alt="logo-blueprint" src={logoNavbar}/></Link>
                             </div>
-                            <div className="navBar__col">
+                            {/* <div className="navBar__col">
                                 <button className="navBar__toggle" onClick={toggleNavbar}>
                                     <span className="navBar__toggle__icon"></span>
                                     <span className="navBar__toggle__icon"></span>
@@ -37,7 +37,7 @@ const Navbar = () => {
                                     <li className="navBar__nav__item"><Link href="#" to="about" smooth={true} duration={500}>About</Link></li>
                                     <li className="navBar__nav__item"><Link href="#" to="contact" smooth={true} duration={500}>Contacto</Link></li>
                                 </ul>
-                            </div>
+                            </div> */}
                         </div>
                     </Col>
                 </Row>
