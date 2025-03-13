9cdfe661abf6621566557623ce03f2890340e023
4me7h <4573r10n@gmail.com>
2025-03-13 00:24:29 -0600

    component: hero and images

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,8 +3,6 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import imgBackground from '../images/img-01.jpg';
-import logo from '../images/logo-icono.png';
-
 import { Zoom } from 'react-awesome-reveal';
 import { useParallax } from 'react-scroll-parallax';
 
@@ -20,12 +18,11 @@ const Hero = () => {
                         <div ref={ref} className="hero__content">
                             <div className='hero__content__header'>
                                 <Zoom triggerOnce cascade>
-                                    <img className="image__logo-icono" src={logo} alt="blueprint-logo"/>     
-                                    <p className='text--lead text--white'>Helping your business grow faster and smarter</p>
-                                    <h1 className="text--title text--white">Turn Clicks Into <span className="lighted lighted--secondary">Quote-Ready</span> Leads With <span className="lighted lighted--secondary">AI-Powered Precision</span>
+                                    <p className='text--lead text--white'>Effortless Business Growth</p>
+                                    <h1 className="text--title text--white">We Generate <span className="lighted lighted--secondary">Quote-Ready</span> Home Improvement <span className="lighted lighted--secondary">Leads for You</span>
                                     </h1>
-                                    <h2 className="text--lead text--white">Blueprint Strategies uses advanced AI to generate and qualify<br /><span className='lighted lighted--secondary'>high-quality leads</span></h2>
-                                    <a className="btn btn--secondary" target="__blank" href="https://wa.me/+17025780098?text=Blueprint%20Strategies%2C%20I%27m%20interested%20in%20growing%20my%20business">Get Started Today</a>
+                                    <h2 className="text--lead text--white">No Heavy Lifting Required!</h2>
+                                    <a className="btn btn--secondary" target="__blank" href="https://wa.me/+17025780098?text=Blueprint%20Strategies%2C%20I%27m%20interested%20in%20growing%20my%20business">Get more leads</a>
                                 </Zoom>
                             </div>
                         </div>
