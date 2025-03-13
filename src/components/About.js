85721662eb6fe72ee0b461eccc19475a0b35446c
4me7h <4573r10n@gmail.com>
2025-03-13 12:19:49 -0600

    component: about / styles: about boxes / icons

--- src/components/About.js
+++ src/components/About.js
@@ -4,13 +4,16 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Gridimg from './Gridimg';
 import { Fade } from 'react-awesome-reveal';
-import { FaRocket, FaBullseye, FaChartLine } from 'react-icons/fa';
+// import { FaRocket, FaBullseye, FaChartLine } from 'react-icons/fa';
 
-import logoLinealIcon from '../images/logo-lineal-icon.png';
+import partners02 from '../images/partners-02.png';
+import icon04 from '../images/icon-04.png';
+import icon05 from '../images/icon-05.png';
+import icon06 from '../images/icon-06.png';
 
 const imagePaths = [  
-  require('../images/img-11.jpg'),
-  require('../images/img-07.jpg'),
+  require('../images/img-02.jpg'),
+  require('../images/img-03.jpg'),
 ];
 
 export default function About() {
@@ -22,10 +25,9 @@ export default function About() {
             <Fade cascade triggerOnce direction="left">
             <div className="about__header">
               <div className="about__header__content">
-                {/* <h3 className="text--subtitle text-center text-uppercase"><em>Who Are <span className="lighted lighted--secondary">We?</span></em></h3> */}
-                <img src={logoLinealIcon} alt="Blueprint Strategies" className="image__logo-about"/>
-                <p className='text--body text-center'>
-                  <strong>Blueprint Strategies</strong> is a forward-thinking agency leveraging AI to help businesses grow faster and smarter. Our mission is to provide not just leads but <strong>real opportunities for success.</strong>
+                <h3 className="text--subtitle text-center text-uppercase text--dark-primary"><em>Who Are <span className="lighted lighted--secondary">We?</span></em></h3>
+                <p className='text--body text-center text--dark-secondary'>
+                  We’re a Las Vegas-based agency that helps home improvement companies secure top-tier, quote-ready leads through strategic advertising and AI-driven automation. Our targeted approach not only frees you up to focus on your craft but also ensures your growth is driven by genuinely interested clients—making your business expansion smooth and efficient.
                 </p>
               </div>
             </div>
@@ -40,32 +42,57 @@ export default function About() {
       <Container>
         <Row>
           <Col>
+            <Fade cascade triggerOnce direction="right">
+              <div className="about__content">
+                <h3 className="text--subtitle text-center text-uppercase text--dark-primary"><em>Our <span className="lighted lighted--secondary">Partners</span></em></h3>
+                <ul className='about__partners'>
+                  <li className='about__partners__item'>
+                    <img src={partners02} alt="MetaAds"/>
+                  </li>
+                  <li className='about__partners__item'>
+                    <img src={partners02} alt="GoHighLevel"/>
+                  </li>
+                  <li className='about__partners__item'>
+                    <img src={partners02} alt="Appointwise"/>
+                  </li>
+                </ul>
+              </div>
+            </Fade>
+          </Col>
+        </Row>
+      </Container>
+      <Container>
+        <Row>
+          <Col>
               <Fade triggerOnce>
                 <div className='headercontent__content'>
-                  <h3 className="text--subtitle text-center text-uppercase"><em>What Makes Us <span className="lighted lighted--secondary">Different</span></em></h3>
+                  <h3 className="text--subtitle text-center text-uppercase text--dark-primary"><em>What Makes Us <span className="lighted lighted--secondary">Different</span></em></h3>
                 </div>
                 <div className="about__content">
                   <ul className="boxes">
                     <li className="boxes__item">
                       <div className="boxes__icon">
-                        <FaRocket size={75}/>
+                        {/* <FaRocket size={75}/> */}
+                        <img src={icon04} alt="icon-01" className="boxes__img"/>
                       </div>
-                      <h4 className="text--lead boxes__title">1. AI-Driven Precision</h4>
-                      <p className='text--body boxes__content'>Automated processes for faster, more accurate results.</p>
+                      <h4 className="text--lead boxes__title">AI-Driven Precision</h4>
+                      <p className='text--body boxes__content'>Our automated processes deliver faster, more accurate results by engaging and qualifying leads instantly.</p>
                     </li>
                     <li className="boxes__item">
                       <div className="boxes__icon">
-                        <FaBullseye size={75}/>
+                        {/* <FaBullseye size={75}/> */}
+                        <img src={icon05} alt="icon-01" className="boxes__img"/>
                       </div>
-                      <h4 className="text--lead boxes__title">2. Custom Solutions</h4>
-                      <p className='text--body boxes__content'>Every campaign is tailored to your specific goals.</p>
+                      <h4 className="text--lead boxes__title">Custom Solutions</h4>
+                      <p className='text--body boxes__content'>Every campaign is tailored to your specific business goals, ensuring you only speak with quote-ready prospects.</p>
                     </li>
                     <li className="boxes__item">
                       <div className="boxes__icon">
-                        <FaChartLine size={75}/>
+                        {/* <FaChartLine size={75}/> */}
+                        <img src={icon06} alt="icon-01" className="boxes__img"/>
                       </div>
-                      <h4 className="text--lead boxes__title">3. Measurable Success</h4>
-                      <p className='text--body boxes__content'>Transparent reporting to track your ROI.</p>
+                      <h4 className="text--lead boxes__title">Measurable Success</h4>
+                      <p className='text--body boxes__content'>Our transparent reporting tools track your ROI and provide clear insights into your lead pipeline.</p>
                     </li>
                   </ul>
                 </div>
