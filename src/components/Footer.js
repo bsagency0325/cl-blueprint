1c66d9a1b54b6a52afd0a9d5a4c0ec89d21701c7
4me7h <4573r10n@gmail.com>
2025-03-13 21:05:26 -0600

    component:footer, copy and icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
+import iconFooter01 from '../images/icon-footer-01.png';
+import iconFooter02 from '../images/icon-footer-02.png';
+import iconFooter03 from '../images/icon-footer-03.png';
+
 const Footer = () => {
     
     const date = new Date();
@@ -7,6 +11,20 @@ const Footer = () => {
 
     return (
         <div className="footer__copy">
+            <ul className="footer__list">
+                <li className='footer__list__item'>
+                    <img src={iconFooter01} alt="icon" className="footer__list__icon"/>
+                    <span className="footer__list__text">Industry Specialists</span>
+                </li>
+                <li className='footer__list__item'>
+                    <img src={iconFooter02} alt="icon" className="footer__list__icon"/>
+                    <span className="footer__list__text">Guaranteed Results</span>
+                </li>
+                <li className='footer__list__item'>
+                    <img src={iconFooter03} alt="icon" className="footer__list__icon"/>
+                    <span className="footer__list__text">Qualified Leads</span>
+                </li>
+            </ul>
             <p className="footer__copy__text">
                 #bluesprintstrategies {year}
             </p>
